6afcbf3ccdda2d751b9b307f24e2310f26609b17
george <georgii.gabaidze@gmail.com>
2024-03-15 02:22:26 +0200

    setup spacing class

---
+++ lib/tokens/spacing.dart
@@ -0,0 +1,15 @@
+class Spacing {
+  static const double smallSpacer = 4.0;
+  static const double spacer = 8.0;
+  static const double defaultPadding = 8.0;
+  static const double averageSpacer = 20.0;
+  static const double spaceBetweenButtons = 12.0;
+  static const double screenPadding = 16.0;
+  static const double underButtonSpacer = 18.0;
+  static const double intervalSpacer = 24.0;
+  static const double spaceAroundScreen = 28.0;
+  static const double upperSpacer = 32.0;
+  static const double largeSpacer = 38.0;
+  static const double helperSpacer = 22.0;
+  static const double bigSpacer = 48.0;
+}
